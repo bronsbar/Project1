dbf654a6403653012130b2e113be7e04e207988a
Bart Bronselaer <Bart@macbook-pro-van-bart.local>
2018-12-13 11:20:46 +0100

    Initial Commit

---
+++ Project1/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Project1
+//
+//  Created by Bart Bronselaer on 13/12/2018.
+//  Copyright © 2018 Bart Bronselaer. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
